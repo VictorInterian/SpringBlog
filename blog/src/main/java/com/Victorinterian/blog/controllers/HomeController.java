31e1534f6f66902c160f0633ad0835d404faeae3
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 16:20:42 -0500

    Se agrego @Autowired

--- blog/src/main/java/com/Victorinterian/blog/controllers/HomeController.java
+++ blog/src/main/java/com/Victorinterian/blog/controllers/HomeController.java
@@ -1,15 +1,34 @@
 package com.victorinterian.blog.controllers;
 
+import com.victorinterian.blog.models.User;
+import com.victorinterian.blog.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
+
+
+    @Autowired
+    private UserService service;
+
     @RequestMapping("/")
     public String index() {
         //return "index";
+
+        User user = new User();
+        user.setFirstName("Victor");
+        user.setLastName("Cavazos");
+        user.setUsername("VictorInterian");
+        user.setPassword("12345");
+
+        service.save(user);
+
+
+
         return "bootstrap";
 
     }
