e662ac32461256ff6e06ab6044ef65bd9e7426c6
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 23:56:46 -0500

    Creacion de archivo UserDetailsService

---
+++ blog/src/main/java/com/Victorinterian/blog/implementations/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.victorinterian.blog.implementations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.victorinterian.blog.models.User;
+import com.victorinterian.blog.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<> ();
+        grantedAuthorities.add(new SimpleGrantedAuthority("user"));
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            grantedAuthorities
+        );
+
+    }
+
+}
