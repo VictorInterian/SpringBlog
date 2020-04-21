a169ed80b92f346f9ae5f71396fe3c7c163f7107
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 23:54:04 -0500

    Creacion donde se guardaran los usuarios

---
+++ blog/src/main/java/com/Victorinterian/blog/controllers/StoreUserForm.java
@@ -0,0 +1,25 @@
+package com.victorinterian.blog.controllers;
+
+public class StoreUserForm {
+
+	public Object getFirstName() {
+		return null;
+	}
+
+	public Object getUserName() {
+		return null;
+	}
+
+	public Object getLastName() {
+		return null;
+	}
+
+	public Object getPassword() {
+		return null;
+	}
+
+	public Object getUsername() {
+		return null;
+	}
+
+}
