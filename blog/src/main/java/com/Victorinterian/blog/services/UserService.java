1d81a10bb0e1bb79299a2b4da5bb258de3c02d0b
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 23:53:15 -0500

    Generacion de sentencia para spring

--- blog/src/main/java/com/Victorinterian/blog/services/UserService.java
+++ blog/src/main/java/com/Victorinterian/blog/services/UserService.java
@@ -1,11 +1,15 @@
 package com.victorinterian.blog.services;
+import java.util.List;
 
 import com.victorinterian.blog.models.User;
 
 public interface UserService {
 
+
+    public List<User> all();
      public User find(Long id);
      public Boolean save(User user);
+	public User findByUsername(String username);
 
 
 }
