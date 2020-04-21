306781b9b6cab532b790ac2731c03e05e9d2a5ab
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 23:04:45 -0500

    Reparacion de issue en BD

--- blog/db.sql
+++ blog/db.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `image_url` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `publiched_at` datetime NULL DEFAULT NULL,
+    `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `created_at` timestamp NULL DEFAULT NULL,
     `updated_at` timestamp NULL DEFAULT NULL,
     `deleted_at` timestamp NULL DEFAULT NULL,
@@ -21,16 +21,14 @@ CREATE TABLE IF NOT EXISTS `articles` (
     `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `body` longtext NOT NULL,
     `imgae_url` varchar(1000) COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-    `publiched_at` datetime NULL DEFAULT NULL,
-    `created_at` datetime NULL DEFAULT NULL,
-    `updated_at` datetime NULL DEFAULT NULL,
-    `deleted_at` datetime NULL DEFAULT NULL,
+    `author_id` int(10) unsigned DEFAULT NULL,
+    `published_at` datetime NULL DEFAULT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `deleted_at` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `articles_slug_unique` (`slug`)
-
-
-
-
-
+    UNIQUE KEY `articles_slug_unique` (`slug`),
+    KEY `articles_author_id_index` (`author_id`),
+    CONSTRAINT `articles_author_id_foreign` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
