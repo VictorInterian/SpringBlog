0891f9251bce9d0eeb3b65bd3cec9818381c73a5
VictorInterian <60495430+VictorInterian@users.noreply.github.com>
2020-04-20 23:13:49 -0500

    Se creo Article.java

--- blog/src/main/java/com/Victorinterian/blog/models/Article.java
+++ blog/src/main/java/com/Victorinterian/blog/models/Article.java
@@ -1,5 +1,131 @@
 package com.victorinterian.blog.models;
 
+import java.sql.Timestamp;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+@Entity
+@Table(name = "articles")
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "slug", nullable = false)
+    private String slug;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(name = "published_at")
+    private Timestamp publishedAt;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+
+    public Long getId() {
+        return id;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String setSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug){
+        this.slug = slug;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body){
+        this.body = body;
+    }
+
+    public Timestamp getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Timestamp publishedAt){
+        this.publishedAt = publishedAt;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt){
+        this.deletedAt = deletedAt;
+    }
+
+    // Relationships
+    public User getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(User author){
+        this.author = author;
+    }
+
+    //Override
+    public String toString() {
+        return "Article [" +
+           "BD ID = " + this.getId() + " " +
+           "Title = " + this.getTitle() + " ]";
+    }
+
+
 
 }
